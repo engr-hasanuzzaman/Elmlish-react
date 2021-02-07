bbe332af932d33e0f6f267b9d4b90723088b8beb
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-07 23:53:55 +0600

    Fefactor the component rederer into smaller component

--- src/App.fs
+++ src/App.fs
@@ -12,42 +12,60 @@ let init() =
       NewTodoTitle = "" 
     }
 
-let render (state: State) (dispatch: Reducer.Actions -> unit) =
+let RenderTodo (todo: Todo) =
   Html.div [
-    Html.h1 [
-      prop.text "My Study topics"
-    ]
-    Html.div [
-      prop.children [
-        for li in state.Todos do
-          Html.div [
-            prop.children [
-              Html.text li.Title
-              Html.input [
-                prop.type' "checkbox"
-                prop.value li.Status
-              ]
-            ]
-          ]
-         
+    prop.classes ["rounded"; "border-blue-500"; "border-2"; "flex"; "h-10"; "items-center"; "pl-5";]
+    prop.children [
+      Html.text todo.Title
+      Html.input [
+        prop.type' "checkbox"
+        prop.value todo.Status
+        prop.classes ["ml-5"]
+        // prop.onChange (fun (e: Browser.Types.HTMLInputElement) -> (MarkComplete li.Id) >> di)
       ]
     ]
-    Html.div [
+  ]
+
+let RenderPageTitle (title: string) = 
+  Html.h1 [
+    prop.classes []
+    prop.text title
+  ]
+
+let RenderTodos (todos: Todo list) (state : State) =
+  Html.div [
+    prop.classes []
+    prop.children [
+       for todo in state.Todos -> RenderTodo todo         
+      ]
+    ]
+
+
+let RenderTodoForm (state : State) (dispatch: Reducer.Actions -> unit) =
+  Html.div [
+    prop.classes []
+    prop.children [
       Html.label [
         prop.text "Input Title"
       ]
-
       Html.input [
         prop.type' "text"
         prop.placeholder "input subject"
         prop.valueOrDefault state.NewTodoTitle
         prop.onTextChange (fun str ->  dispatch ( UpdateTitle str ))
-      ] 
+      ]
+      Html.button [
+        prop.text "add new task"
+        prop.onClick (fun _ -> dispatch (AddNew state.NewTodoTitle) )
+      ]
     ]
-    Html.button [
-      prop.text "add new task"
-      prop.onClick (fun _ -> dispatch (AddNew state.NewTodoTitle) )
-    ]
+  ]
+    
+let render (state: State) (dispatch: Reducer.Actions -> unit) =
+  Html.div [
+    RenderPageTitle "Todos"
+    RenderTodos state.Todos state
+    RenderTodoForm state dispatch
   ]
 
 Program.mkSimple init Reducer.update render
--- src/Update.fs
+++ src/Update.fs
@@ -13,7 +13,7 @@ let update (action: Actions) (state: AppState.State): AppState.State =
     match action with
     | AddNew title ->
         Fable.Core.JS.console.log("AddNew has been called with ", title)
-        { state with Todos = state.Todos @ [{ Title = "sumon"; Status = false; Id = (AppState.TodoId (System.Guid.NewGuid( )) ) }] }
+        { state with Todos = state.Todos @ [{ Title = title; Status = false; Id = (AppState.TodoId (System.Guid.NewGuid( )) ) }] }
     | MarkComplete Id ->
         { state with Todos = List.filter (fun todo -> todo.Id <> Id) state.Todos}
     | UpdateTitle title -> { state with NewTodoTitle = title }

9b8dbdd131ddf02f9630e8c6fa5ccd474e7dfb00
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-11 00:09:18 +0600

    Delete functionality for todo

--- src/components/Index.fs
+++ src/components/Index.fs
@@ -16,17 +16,18 @@ let RenderTodo (todo: Todo) (dispatch: Reducer.Actions -> unit) =
           Html.button [
             prop.classes ["flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"]
             prop.text "Done"
-            prop.onClick (fun (e) -> dispatch (ToggleStatus todo.Id))
+            prop.onClick (fun (e) -> (ToggleStatus todo.Id) |> dispatch )
           ]
       else
           Html.button [
             prop.classes ["flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey"]
             prop.text "Not Done"
-            prop.onClick (fun (e) -> dispatch (ToggleStatus todo.Id))
+            prop.onClick (fun _ -> (ToggleStatus todo.Id) |> dispatch)
           ]
       Html.button [
         prop.classes ["flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"]
         prop.text "Remove"
+        prop.onClick (fun _ -> (DeleteTodo todo.Id) |> dispatch)
       ]
     ]
   ]
